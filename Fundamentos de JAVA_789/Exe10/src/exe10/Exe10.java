2930d6137e57763215ac4791a98fea306208a290
hugoresende27 <Hugoresende27@gmail.com>
2021-12-14 16:00:39 +0000

    a

--- Fundamentos de JAVA_789/Exe10/src/exe10/Exe10.java
+++ Fundamentos de JAVA_789/Exe10/src/exe10/Exe10.java
@@ -1,26 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exe10;
 
 import java.util.Scanner;
-
 /**
  *
  * @author Hugo
  */
 
-
 public class Exe10 {
 
     /**
-     *Desenvolva um programa que permita determinar a se o aluno está aprovado ou reprovado tendo 
-     * em conta as seguintes condições:
+*Desenvolva um programa que permita determinar a se o aluno está aprovado ou reprovado tendo 
+* em conta as seguintes condições:
 Média dos 3 testes >=10
 Número de faltas <=3
-
      */
     public static void main(String[] args) {
         // TODO code application logic here
@@ -39,12 +32,10 @@ Número de faltas <=3
         media=notas/3;
         System.out.printf("\nMEDIA:: %.2f\n",media);
         
-        if (media>=10 && faltas <= 3){
+        if (media>=10 || faltas <= 3){
             System.out.println("APROVADO!");
         }else {
             System.out.println("REPROVADO! ");
-        }
-        
+        }  
     }
-    
 }

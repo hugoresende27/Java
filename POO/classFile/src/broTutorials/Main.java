725a927c86b7b8ae88e595f09d70239c43bdabb2
hugoresende27 <Hugoresende27@gmail.com>
2021-12-30 18:32:42 +0000

    a

---
+++ POO/classFile/src/broTutorials/Main.java
@@ -0,0 +1,12 @@
+package broTutorials;
+/*
+The File class from the java.io package, allows us to work with files.
+To use the File class, create an object of the class, and specify the filename or directory name:
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

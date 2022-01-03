e3a2716bcd0625bc89707984fd790cd61eb01783
hugoresende27 <Hugoresende27@gmail.com>
2022-01-03 20:25:16 +0000

    a

---
+++ GUI/JRadioButton/src/BroTutorials/Main.java
@@ -0,0 +1,9 @@
+package BroTutorials;
+//JRadioButton = um ou mais botões em grupo apenas 1 pode ser selecionado
+
+public class Main {
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
---
+++ GUI/JRadioButton/src/BroTutorials/MyFrame.java
@@ -0,0 +1,50 @@
+package BroTutorials;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JRadioButton pizzaBtn;
+    JRadioButton hambBtn;
+    JRadioButton hotdogBtn;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        pizzaBtn = new JRadioButton("pizza");
+        hambBtn = new JRadioButton("Hamburguer");
+        hotdogBtn = new JRadioButton("Hot Dog");
+
+        ButtonGroup grupoBtn = new ButtonGroup();
+        grupoBtn.add(pizzaBtn);
+        grupoBtn.add(hambBtn);
+        grupoBtn.add(hotdogBtn);
+
+        this.add(pizzaBtn);
+        this.add(hambBtn);
+        this.add(hotdogBtn);
+
+        pizzaBtn.addActionListener(this);
+        hambBtn.addActionListener(this);
+        hotdogBtn.addActionListener(this);
+
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+            if(e.getSource()==pizzaBtn){
+                System.out.println("Pediu Pizza!");
+            } else if (e.getSource()==hambBtn){
+                System.out.println("Pediu um Hamburguer!");
+            } else if (e.getSource()==hotdogBtn){
+                System.out.println("Pediu um Hot Dog!");
+            }
+    }
+}

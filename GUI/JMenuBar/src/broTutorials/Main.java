1f6fe8b9a0965e9c5e4a8cc5166be723061ea41e
hugoresende27 <Hugoresende27@gmail.com>
2022-01-05 12:32:35 +0000

    a

---
+++ GUI/JMenuBar/src/broTutorials/Main.java
@@ -0,0 +1,14 @@
+package broTutorials;
+/*
+The JMenuBar class is used to display menubar on the window or frame. It may have several menus.
+The object of JMenu class is a pull down menu component which is displayed from the menu bar. It inherits the JMenuItem class.
+The object of JMenuItem class adds a simple labeled menu item. The items used in a menu must belong to the JMenuItem or any of
+its subclass.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        new MyFrame();
+    }
+}
---
+++ GUI/JMenuBar/src/broTutorials/MyFrame.java
@@ -0,0 +1,86 @@
+package broTutorials;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+    JMenuItem loadItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+
+    ImageIcon loadIcon;
+    ImageIcon saveIcon;
+    ImageIcon exitIcon;
+
+    MyFrame(){
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+
+        loadIcon = new ImageIcon("");
+
+        ///CRIAR JMENUBAR
+        JMenuBar menu1 = new JMenuBar();
+
+        //CRIAR JMENU SEPARADORES
+        JMenu fileMenu = new JMenu("Ficheiro");
+        JMenu editMenu = new JMenu("Editar");
+        JMenu helpMenu = new JMenu("Ajuda");
+
+        //CRIAR ITEMS DOS SEPARADORES
+        loadItem = new JMenuItem("Carregar");
+        saveItem = new JMenuItem("Guardar");
+        exitItem = new JMenuItem("Sair");
+
+        //ACTIONLISTENER DE OPÇOES DO MENU COM this
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        //ADICIONAR ATALHOS DE TECLAS AO MENUS
+        loadItem.setMnemonic(KeyEvent.VK_L); //L para load
+        saveItem.setMnemonic(KeyEvent.VK_T); //F5 para save
+        exitItem.setMnemonic(KeyEvent.VK_S); //L para exit
+
+        //ATALHOS DOS SEPARADORES --> TECLA ALT + ATALHO
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        editMenu.setMnemonic(KeyEvent.VK_E);
+        helpMenu.setMnemonic(KeyEvent.VK_F9);
+
+        //ADICIONAR OS ITEMS AO SEPARADOR filemenu
+        fileMenu.add(loadItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+
+        //ADICIONAR OS SEPARADORES AO MENU
+        menu1.add(fileMenu);
+        menu1.add(editMenu);
+        menu1.add(helpMenu);
+
+        //ADICIONAR O JMENUBAR À JANELA
+        this.setJMenuBar(menu1);
+        this.setVisible(true);
+
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource()==loadItem){
+            System.out.println("Load de ficheiro");
+        } else if (e.getSource()==saveItem){
+            System.out.println("Save do ficheiro");
+        } else {
+            System.exit(0); //exit do sistema
+        }
+
+    }
+}

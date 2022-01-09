43d11b96f7030a2ad5c408dda43da2cd3b3e42a7
hugoresende27 <Hugoresende27@gmail.com>
2022-01-09 12:41:37 +0000

    a

---
+++ encryptionProgram_OTP/src/broTutorials/Main.java
@@ -0,0 +1,15 @@
+package broTutorials;
+/*
+Java Programming Java 8Object Oriented Programming. Generate OTP is now a requirement on most of the website now-a-days.
+In case of additional authentication, system generates a OTP password adhering to OTP policy of the company.
+OTP means One Time Password: it's a temporary, secure PIN-code sent to you via SMS or e-mail that is valid only for one
+session. ... If you cannot receive and confirm the OTP code, you will not be able to continue with your account registration.
+ASCII - American Standart Code for Information Interchange
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

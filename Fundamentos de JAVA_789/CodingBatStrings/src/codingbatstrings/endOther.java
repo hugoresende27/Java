2aaa7dfb9de66cfafb33d9466986a0395f4f45cb
hugoresende27 <Hugoresende27@gmail.com>
2022-01-10 12:45:46 +0000

    a

--- Fundamentos de JAVA_789/CodingBatStrings/src/codingbatstrings/endOther.java
+++ Fundamentos de JAVA_789/CodingBatStrings/src/codingbatstrings/endOther.java
@@ -8,12 +8,25 @@ endOther("AbC", "HiaBc") → true
 endOther("abc", "abXabc") → true
  */
 
-public class endOther {
-    public boolean endOther(String a, String b) {
+import java.util.Locale;
 
+public class endOther {
+    public static boolean endOther(String a, String b) {
+
+        String strA = a.toLowerCase();
+        String strB = b.toLowerCase();
+        int tamanhoA = strA.length();
+        int tamanhoB = strB.length();
+
+        if (strA.endsWith(strB) || strB.endsWith(strA)){
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        
+
+        System.out.println(endOther("Hiabc", "abc") );
+        System.out.println(endOther("AbC", "HiaBc") );
     }
 }

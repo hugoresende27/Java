5f4cff41529e05f06e527adbaa1b0bd738a63d60
hugoresende27 <Hugoresende27@gmail.com>
2022-01-11 18:20:18 +0000

    a

--- ClockApp/src/broTutorials/StopWatch.java
+++ ClockApp/src/broTutorials/StopWatch.java
@@ -20,6 +20,24 @@ public class StopWatch implements ActionListener {
     String minStr = String.format("%02d",minQtd);
     String hoursStr = String.format("%02d",hoursQtd);
 
+    Timer timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            timeQtd += 1000;
+            hoursQtd = (timeQtd/3600000);
+            minQtd = (timeQtd/60000) % 60;
+            secQtd = (timeQtd/1000) % 60;       //%60 para não mostrar valores acima de 60
+
+            segsStr = String.format("%02d",secQtd);
+            minStr = String.format("%02d",minQtd);
+            hoursStr = String.format("%02d",hoursQtd);
+
+            timeLlb.setText(hoursStr+ ":" +minStr + ":" + segsStr);
+
+        }
+    });
+
     StopWatch(){
 
         timeLlb.setText(hoursStr + ":" + minStr + ":" + segsStr);
@@ -29,12 +47,26 @@ public class StopWatch implements ActionListener {
         timeLlb.setOpaque(true);
         timeLlb.setHorizontalAlignment(JTextField.CENTER);
 
+        startBtn.setBounds(100,200,100,50);
+        startBtn.setFont(new Font("Ink Free",Font.PLAIN,20));
+        startBtn.setFocusable(false);
+        startBtn.addActionListener(this);
+
+        resetBtn.setBounds(200,200,100,50);
+        resetBtn.setFont(new Font("Ink Free",Font.PLAIN,20));
+        resetBtn.setFocusable(false); //border q fica no botão se true
+        resetBtn.addActionListener(this);
+
+        frame.add(startBtn);
+        frame.add(resetBtn);
+
         frame.add(timeLlb);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);//sem layout manager
         frame.setTitle("StopWatch HR@2022");
+        frame.setLocationRelativeTo(null);
 
 
         frame.setVisible(true);
@@ -43,21 +75,50 @@ public class StopWatch implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==startBtn){
+
+            if (started == false){
+                started=true;
+                startBtn.setText("stop");
+                start();
+            }
+            else {
+                started=false;
+                startBtn.setText("start");
+                stop();
+            }
+        }
+
+        if (e.getSource()==resetBtn){
+            started=false;
+            startBtn.setText("start");
+            reset();
+        }
 
     }
 
     ///////////STOP////////////////
     void start(){
-
+        timer.start();
     }
 
     ///////////STOP////////////////
     void stop(){
-
+        timer.stop();
     }
 
     ///////////RESET////////////////
     void reset(){
+        timer.stop();
+        timeQtd=0;
+        secQtd=0;
+        minQtd=0;
+        hoursQtd=0;
 
+        segsStr = String.format("%02d",secQtd);
+        minStr = String.format("%02d",minQtd);
+        hoursStr = String.format("%02d",hoursQtd);
+
+        timeLlb.setText(hoursStr+ ":" +minStr + ":" + segsStr);
     }
 }

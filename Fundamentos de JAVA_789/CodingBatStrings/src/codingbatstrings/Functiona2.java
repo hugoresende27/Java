24a3d41d984220df851dd9b3de6753c3e95926d7
hugoresende27 <Hugoresende27@gmail.com>
2022-01-11 14:50:45 +0000

    a

---
+++ Fundamentos de JAVA_789/CodingBatStrings/src/codingbatstrings/Functiona2.java
@@ -0,0 +1,22 @@
+package codingbatstrings;
+/*
+Given a list of strings, return a list of the strings, omitting any string that contains a "z".
+(Note: the str.contains(x) method returns a boolean)
+noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+noZ(["hziz", "hzello", "hi"]) → ["hi"]
+noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Functiona2 {
+
+    public List<String> noZ(List<String> strings) {
+        return strings;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String>arr1= new ArrayList<String>("aaa", "bbb", "aza");
+    }
+}
